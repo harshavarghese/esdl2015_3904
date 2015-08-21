b559805527261ccda8faaf28a51962ad450e9a76
harshavarghese <harshavarghese18@gmail.com>
2015-08-21 14:35:13 +0530

    the file was just changed

--- calc.cpp
+++ calc.cpp
@@ -1,4 +1,5 @@
 //This is a program for implementing calulator. 
-//We are performing this for esdl lab.
+
 //The operations we are about to perform include:
 //addition,subtraction,multiplication and division.
+//enjoy!!
