7f9b0ca2d0ba9f5152816ad52d3e99532b00b268
harshavarghese <harshavarghese18@gmail.com>
2015-08-21 14:31:52 +0530

    hello!

---
+++ calc.cpp
@@ -0,0 +1,4 @@
+This is a program for implementing calulator. 
+We are performing this for esdl lab.
+The operations we are about to perform include:
+addition,subtraction,multiplication and division.
